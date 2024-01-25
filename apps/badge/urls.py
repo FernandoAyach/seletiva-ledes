bbdf7071fcfff4e6d8be469510c200708b521db9
mshynji <m.shynjirobbie@gmail.com>
2024-01-25 14:32:48 -0400

    [ADD] Admin page to approve/reject badge info. changes

--- apps/badge/urls.py
+++ apps/badge/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.badge.views import badge
+from apps.badge.views import badge, admin
 
 urlpatterns = [
     path('', badge, name = 'badge'),
+    path('admin/', admin, name = 'admin'),
 ]
